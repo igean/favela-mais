c7d47449eff8befe61335882f2c2e25e3f850f73
Gean Araujo <icrazy.gean@gmail.com>
2020-12-12 21:06:50 -0300

    colo

--- src/Form.js
+++ src/Form.js
@@ -9,7 +9,7 @@ class Login extends React.Component {
 
     render() {
         return (
-            <form>
+            <form action="/inicio">
                 <label>
                     CPF:
                     <div className="input-block">
--- src/dash.js
+++ src/dash.js
@@ -7,20 +7,23 @@ class Dash extends React.Component{
         return (
             <div>
                 <header className="dash">
-                    <h2>Olá <br /> Renan Santana</h2>
+                    <h2><span className="text-o">Olá</span> <br /> Renan Santana</h2>
+                    <div className="circle">
+                        <i className="fas fa-question" />
+                    </div>
                 </header>
                 <main className="dash">
-                    <p>Nosso intuito é unir você CAMPEÃO que busca sucesso financeiro, ou até aprender uma nova arte, tecnologia, empreendimento.</p>
+                    <p>Nosso intuito é unir você <span className="text-o">CAMPEÃO</span> que busca sucesso financeiro, ou até aprender uma nova arte, tecnologia, empreendimento.</p>
 
-                    <p>Somos uma plataforma filantrópica que tem como objetivo IMPACTAR e dar visibilidade a toda comunidade, e tornar em um espaço para todos.</p>
+                    <p>Somos uma plataforma filantrópica que tem como objetivo <span className="bold">IMPACTAR</span> e dar visibilidade a toda comunidade, e tornar em um espaço para todos.</p>
 
                     <div className="menu">
                         <strong>Navegue pelas opções abaixo</strong>
                         <div className="items">
-                        <div className="item"></div>
-                        <div className="item"></div>
-                        <div className="item"></div>
-                        <div className="item"></div>
+                        <div className="item"><i className="fas fa-home fa-3x" /></div>
+                        <div className="item"><i className="fas fa-users fa-3x" /></div>
+                        <div className="item"><i className="fas fa-book-reader fa-3x" /></div>
+                        <div className="item"><i className="fas fa-user-circle fa-3x" /></div>
                         </div>
                     </div>
                 </main>
