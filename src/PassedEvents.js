99e000e14aec648aca2a808cf29a5ba97e9d2bd2
Gean Araujo <icrazy.gean@gmail.com>
2020-12-13 20:08:29 -0300

    events page done

---
+++ src/PassedEvents.js
@@ -0,0 +1,85 @@
+const PassedEvents = () => {
+  return (
+    <div>
+      <h3>Eventos passados</h3>
+      <div className="events-cards">
+        <div className="card">
+          <div className="img-container">
+            <img src="https://images.unsplash.com/photo-1570715431238-e70313e87b8a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=625&q=80" />
+          </div>
+          <div className="card-info">
+            <strong className="card-title">União 1.0</strong>
+            <span>12/12/2020</span>
+          </div>
+          <div className="card-body">
+            <p>
+              <i className="fas fa-user" />
+              Antonio Nunes
+            </p>
+            <p>
+              Esse evento visa unir as comunidades para a arrecadação de roupas
+              para doação
+            </p>
+            <p>CONFIRMADO: 85 pessoas</p>
+
+            <div>
+              <button className="went">Eu fui</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="img-container">
+            <img src="https://images.unsplash.com/photo-1570715431238-e70313e87b8a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=625&q=80" />
+          </div>
+          <div className="card-info">
+            <strong className="card-title">União 1.0</strong>
+            <span>12/12/2020</span>
+          </div>
+          <div className="card-body">
+            <p>
+              <i className="fas fa-user" />
+              Antonio Nunes
+            </p>
+            <p>
+              Esse evento visa unir as comunidades para a arrecadação de roupas
+              para doação
+            </p>
+            <p>CONFIRMADO: 85 pessoas</p>
+
+            <div>
+              <button className="went">Eu fui</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="img-container">
+            <img src="https://images.unsplash.com/photo-1570715431238-e70313e87b8a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=625&q=80" />
+          </div>
+          <div className="card-info">
+            <strong className="card-title">União 1.0</strong>
+            <span>12/12/2020</span>
+          </div>
+          <div className="card-body">
+            <p>
+              <i className="fas fa-user" />
+              Antonio Nunes
+            </p>
+            <p>
+              Esse evento visa unir as comunidades para a arrecadação de roupas
+              para doação
+            </p>
+            <p>CONFIRMADO: 85 pessoas</p>
+
+            <div>
+              <button className="went">Eu fui</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PassedEvents;
