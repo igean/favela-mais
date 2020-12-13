fc4dfeb34122baf4b2e0db9029d313bf1a201d46
Gean Araujo <icrazy.gean@gmail.com>
2020-12-13 16:59:39 -0300

    something with layout

--- src/EducPage.js
+++ src/EducPage.js
@@ -8,7 +8,49 @@ const EducPage = () => {
       É hora de aprender, e para isso nada melhor que estudar aquele conteúdo feito com carinho pelos nossos profissionais. Quem sai <span className="text-o">VENCEDOR</span> é você!
       </p>
 
-      
+      <div className="courses">
+        <strong>Veja o que preparamos para você</strong>
+        <div className="free courses-container">
+            <span>Os cursos abaixo são gratuitos</span>
+            <div className="course">
+              <strong>EMPREENDENDO COM A COMUNIDADE</strong>
+              <p>O empreendedorismo é a melhor arma contra o desemprego. Com ele podemos criar, inovar, alterar toda nossa vida.</p>
+              <div className="enter-course">
+                <i class="fas fa-unlock fa-2x" />
+                <button>Estou interessado</button>
+              </div>
+            </div>
+            <div className="course">
+              <strong>EMPREENDENDO COM A COMUNIDADE</strong>
+              <p>O empreendedorismo é a melhor arma contra o desemprego. Com ele podemos criar, inovar, alterar toda nossa vida.</p>
+              <div className="enter-course">
+                <i class="fas fa-unlock fa-2x" />
+                <button>Estou interessado</button>
+              </div>
+            </div>
+        </div>
+
+        <div className="payed courses-container">
+            <span>Os cursos abaixo são pagos</span>
+            <div className="course">
+              <strong>EMPREENDENDO COM A COMUNIDADE</strong>
+              <p>O empreendedorismo é a melhor arma contra o desemprego. Com ele podemos criar, inovar, alterar toda nossa vida.</p>
+              <div className="enter-course">
+                <i class="fas fa-unlock fa-2x" />
+                <button>Estou interessado</button>
+              </div>
+            </div>
+            <div className="course">
+              <strong>EMPREENDENDO COM A COMUNIDADE</strong>
+              <p>O empreendedorismo é a melhor arma contra o desemprego. Com ele podemos criar, inovar, alterar toda nossa vida.</p>
+              <div className="enter-course">
+                <i class="fas fa-unlock fa-2x" />
+                <button>Estou interessado</button>
+              </div>
+            </div>
+        </div>
+        
+      </div>
     </div>
   );
 };
